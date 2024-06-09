4da60853f4eb53198d6d23e45279210bd6aa1490
Nesta Bentum <nesta.bentum@gmail.com>
2024-06-09 16:22:10 +0200

    Improve Stuff

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:17-jdk-slim
+FROM openjdk:21-jdk-slim
 
 COPY target/server.jar /home/test.jar
 COPY cloud-vsum /cloud-vsum
--- src/edu/kit/ipd/sdq/vitruvius/cloud/server/launch/ServerLaunch.java
+++ src/edu/kit/ipd/sdq/vitruvius/cloud/server/launch/ServerLaunch.java
@@ -1,12 +1,14 @@
 package edu.kit.ipd.sdq.vitruvius.cloud.server.launch;
 
 import java.io.IOException;
+import org.eclipse.emf.edit.command.SetCommand;
 
 public class ServerLaunch {
 
 	public static void main(String[] args) throws IOException {
 		ServerStarter starter = new ServerStarter();
 		starter.startServer();
+		var test = SetCommand.class;
 	}
 
 }
